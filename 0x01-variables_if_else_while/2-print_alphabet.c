187b12ea72ce5fb2c1534e8277ac1f3a74c7505d
Oluwaseun Adekitan <seunjohn83@gmail.com>
2022-09-09 22:04:27 -0700

    n

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(void)  
 {
-	char low;
+  
+  char low;
+  
+  for (low = 'a'; low <= 'z'; low++)
+    
+    putchar(low);
+  
+  putchar('\n');
+  
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+  
+  return (0);
+  
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -12,9 +12,11 @@ int main(void)
 {
 	char low;
 
-	for(low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
+	for (low = 'z'; low >= 'a'; low--)
+	  {
+	    putchar(low);
+	  }
+	putchar('\n');
 
 	return (0);
 }
