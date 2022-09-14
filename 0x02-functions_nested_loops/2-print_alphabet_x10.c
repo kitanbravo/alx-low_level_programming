4c42884d2c6514af04a16d00eeea0e7646297228
Oluwaseun Adekitan <seunjohn83@gmail.com>
2022-09-13 20:25:52 -0700

    print 10 times the alpharbet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Check the code
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int y;
+
+	for (y = 0; y < 10; j++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
