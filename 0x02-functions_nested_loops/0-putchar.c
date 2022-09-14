1edfced54bd02353056b483c4c9753a81cf346a3
Oluwaseun Adekitan <seunjohn83@gmail.com>
2022-09-13 19:21:56 -0700

    another correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <main.h>
 /**
 *main - Entry point
 *Return: always 0 (successs)
 */
-char main(void)
+int main(void)
 {
-	puts("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
 
 putchar('\n');
 
