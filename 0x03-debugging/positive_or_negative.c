b4e8f6b895ead6b34202a5da5e8416effdbc6c1d
Oluwaseun Adekitan <seunjohn83@gmail.com>
2022-09-15 14:20:19 -0700

    correction

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,13 +1,13 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int positive_or_negative(void)
 {
 	int n;
 
