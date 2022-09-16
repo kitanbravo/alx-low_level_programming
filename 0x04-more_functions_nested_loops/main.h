54a5626142a27e54847b136ddc958c4e3c4d8506
Oluwaseun Adekitan <seunjohn83@gmail.com>
2022-09-15 18:22:57 -0700

    another commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -11,12 +13,18 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+void print_number(int n);
+
 void print_most_numbers(void);
 
 void more_numbers(void);
 
 void print_line(int n);
 
-int _putchar(char c);
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
 
 #endif
