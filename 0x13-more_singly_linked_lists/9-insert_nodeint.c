fc4e8b7afc411aeb54a577865f54845098929926
Oluwaseun Adekitan <seunjohn83@gmail.com>
2022-10-26 00:10:22 +0000

    fixed code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,49 +1,100 @@
 #include "lists.h"
+#include <stdlib.h>
+#define node listint_t
+
 
 /**
- * insert_nodeint_at_index - A function that inserts node at given index
- * @head: A pointer to listint_t structure
- * @idx: The index of the list
- * @n: An integer data for new node
- * Return: The address to new node at specified index, or NULL if it failed
+ * get_size - returns the number of elements in a linked list
+ * @h: head of the list
+ * Return: number of elements in the list
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+size_t get_size(const listint_t *h)
 {
-	unsigned int i = 1;
-	listint_t *new_node, *temp;
+	if (h == (void *)0)
+		return (0);
+	return (1 + get_size(h->next));
+}
 
+/**
+ * get_node - returns the node at a particular index
+ *			in the list
+ * @head: the head of the list
+ * @index: the index of the node, starting from 0
+ *
+ * Return: node at that index, else NULL
+ */
+node *get_node(node *head, unsigned int index)
+{
 	if (head == NULL)
 		return (NULL);
+	if (index == 0)
+		return (head);
+	return (get_node(head->next, index - 1));
+}
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+/**
+ * insert_nodeint_at_index - inserts a node at a given index
+ *
+ * @head: the start of the list
+ * @idx: the idx at which the new node should be inserted
+ * @n: an integer
+ *
+ * Return: the new element, or NULL if it failed
+ */
+node *insert_nodeint_at_index1(node **head, unsigned int idx, int n)
+{
+	node *prev, *temp, *new;
+
+	if (idx > get_size(*head))
 		return (NULL);
-
-	if (*head == NULL)
+	if (idx == 0)
 	{
-		*head = new_node;
-		new_node->next = NULL;
-		new_node->n = n;
-		return (new_node);
+		temp = *head;
+		prev = NULL;
 	}
+	else
+	{
+		prev = get_node(*head, idx - 1);
+		temp = prev->next;
+	}
+	new = malloc(sizeof(node));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	prev->next = new;
+	new->next = temp;
+	return (new);
+}
+
+node *insert_nodeint_at_index(node **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	node *temp, *new;
+
+	temp = *head;
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		new_node->n = n;
-		*head = new_node;
-		return (new_node);
+		new = malloc(sizeof(node));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-
-	temp = *head;
-	while (i < idx)
+	for (i = 0; i < idx - 1; i++)
 	{
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
 		temp = temp->next;
-		i++;
 	}
-
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+	new = malloc(sizeof(node));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
 }
+
